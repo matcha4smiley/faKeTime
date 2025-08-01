6cca85ae1dc1698603175725d448d503e8425a6e
matcha4smiley <matcha4smiley@gmail.com>
2025-08-01 13:14:57 +0900

    Fix publish.yml & build.gradle.kts

--- faketime/build.gradle.kts
+++ faketime/build.gradle.kts
@@ -27,18 +27,41 @@ publishing {
             groupId = "io.github.matcha4smiley"
             artifactId = "faketime"
             version = "0.1.0"
+
+            pom {
+                name.set("faKeTime")
+                description.set("A Kotlin library to fake time during testing.")
+                url.set("https://github.com/matcha4smiley/faKeTime")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("matcha4smiley")
+                        name.set("matcha4smiley")
+                        email.set("matcha4smiley@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/matcha4smiley/faKeTime.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:matcha4smiley/faKeTime.git")
+                    url.set("https://github.com/matcha4smiley/faKeTime")
+                }
+            }
         }
     }
+
     repositories {
         maven {
             name = "centralPortal"
-            url = uri("https://central.sonatype.com/api/v1/publisher/deployments/download/")
-            credentials(HttpHeaderCredentials::class) {
-                name = System.getenv("CENTRAL_AUTH_HEADER_NAME")
-                value = System.getenv("CENTRAL_AUTH_HEADER_VALUE")
-            }
-            authentication {
-                create("header", HttpHeaderAuthentication::class)
+            url = uri("https://central.sonatype.com/api/v1/publisher/deployments")
+            credentials {
+                username = System.getenv("CENTRAL_PORTAL_USERNAME")
+                password = System.getenv("CENTRAL_PORTAL_PASSWORD")
             }
         }
     }

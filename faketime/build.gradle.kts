fd44a5e62423aed41b1ca59ff88d10edb42b86a2
matcha4smiley <matcha4smiley@gmail.com>
2025-08-02 02:01:28 +0900

    Fix publish settings

--- faketime/build.gradle.kts
+++ faketime/build.gradle.kts
@@ -1,6 +1,4 @@
-import org.jreleaser.gradle.plugin.JReleaserExtension
-
-// faketime-core/build.gradle.kts
+// faketime/build.gradle.kts
 plugins {
     kotlin("jvm") version "1.9.10"
     `maven-publish`
@@ -27,51 +25,4 @@ tasks.test {
 java {
     withSourcesJar()
     withJavadocJar()
-}
-
-publishing {
-
-    publications {
-        create<MavenPublication>("faketime") {
-            from(components["java"])
-            groupId = "io.github.matcha4smiley"
-            artifactId = "faketime"
-            version = "0.1.0"
-
-            pom {
-                name.set("faKeTime")
-                description.set("A Kotlin/Java library to fake or freeze time for testing")
-                url.set("https://github.com/matcha4smiley/faKeTime")
-                inceptionYear.set("2025")
-                licenses {
-                    license {
-                        name.set("Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("matcha4smiley")
-                        name.set("matcha4smiley")
-                        email.set("matcha4smiley@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/matcha4smiley/faKeTime.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/matcha4smiley/faKeTime.git")
-                    url.set("https://github.com/matcha4smiley/faKeTime")
-                }
-            }
-        }
-    }
-    repositories {
-        maven {
-            name = "localStaging"
-            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
-        }
-    }
-}
-
-extensions.configure<JReleaserExtension>("jreleaser") {
-    configFile.set(rootProject.file("jreleaser.yml"))
 }

9a61980118be96ea46289b07057cd59c292d3eae
matcha4smiley <matcha4smiley@gmail.com>
2025-08-06 00:17:27 +0900

    spotless apply

--- faketime/src/test/kotlin/io/github/matcha4smiley/faketime/datetime/FakeDateTimeTest.kt
+++ faketime/src/test/kotlin/io/github/matcha4smiley/faketime/datetime/FakeDateTimeTest.kt
@@ -5,9 +5,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class FakeDateTimeTest {
-
     @Test
-    fun `fixed returns correct instant and unixTime`()  {
+    fun `fixed returns correct instant and unixTime`() {
         val instant = Instant.parse("2025-07-30T12:00:00Z")
         val fake = FakeDateTime.fixed(instant)
 
